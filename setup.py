937171c53ec61f7496838916ecdf7ad35cf46877
w4ffl35 <w4ffl35@ml1.net>
2023-03-26 18:42:16 -0600

    fixes setup files

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='chatai',
-    version='1.0.2',
-    description='A python library for AI',
+    version='1.0.5',
     author='Capsize LLC',
+    description='Chat AI: A chatbot framework',
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="",
+    keywords="ai, chatbot, chat, ai",
+    license="AGPL-3.0",
+    author_email="contact@capsize.gg",
+    url="https://github.com/w4ffl35/chatai",
+    package_dir={"": "chatai"},
+    packages=find_packages("chatai"),
+    include_package_data=True,
+    python_requires=">=3.10.0",
     install_requires=[
         "aihandler==1.8.10",
     ]
