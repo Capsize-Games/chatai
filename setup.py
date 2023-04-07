f7a7b761661ca108ef05e76703ea49e9202359af
w4ffl35 <w4ffl35@ml1.net>
2023-04-07 05:01:59 -0600

    bump to v1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chatairunner',
-    version='1.0.12',
+    version='1.1.0',
     author='Capsize LLC',
     description='Chat AI: A chatbot framework',
     long_description=open("README.md", "r", encoding="utf-8").read(),
